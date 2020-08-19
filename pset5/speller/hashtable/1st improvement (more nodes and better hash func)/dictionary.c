a1f8b3358d5513ba0dd26b026e9034e8bcec1643
Mariano <mcoutada@gmail.com>
2020-08-19 12:13:04 -0300

    ...

--- pset5/speller/hashtable/1st improvement (more nodes and better hash func)/dictionary.c
+++ pset5/speller/hashtable/1st improvement (more nodes and better hash func)/dictionary.c
@@ -8,24 +8,34 @@
 #include <strings.h> //this is needed for strcasecmp
 #include <stdlib.h>  //this is needed for malloc
 
-// Represents a node in a hash table
 typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
 } node;
 
-// Number of buckets in hash table
-// One bucket per character in alphabet
-const unsigned long long N = 1000000;
+/* english has 230k words approx, it's better to have a 6 digits hash to diminish the size of a node's linked list
+   from what I read hash functions improve if you use the biggest prime number possible, for 6 digits is 999983
+*/
+#define N 999999
 
-// Hash table
 node *table[N];
 
-//cnt is unsigned (positive) because it's a counter and it's what CS50 wants us to return in size()
 unsigned int dic_wrds_cnt = 0;
 
-// Returns true if word is in dictionary else false
+unsigned int hash(const char *s)
+{
+
+    // djb2a hash  Ref: http://www.cse.yorku.ca/~oz/hash.html
+    unsigned long hash = 5381;
+    while (*s)
+    {
+        hash = (((hash << 5) + hash) ^ *s++); //  hash = hash * 33 + c;  // you can also try this version
+        hash = hash ^ (hash >> 16);
+    }
+    return hash % N;
+}
+
 bool check(const char *word)
 {
     int len = strlen(word);
@@ -37,15 +47,12 @@ bool check(const char *word)
         lookup_wrd[i] = tolower(word[i]);
     }
 
-    //words need to be nul-terminated for strcasecmp to work, also the hash changes
-
     lookup_wrd[len] = '\0';
 
     node *node_ptr = table[hash(lookup_wrd)];
 
     while (node_ptr != NULL)
     {
-        // strcmp(wrd1, wrd2) == 0 means wrd1 = wrd2, < 0 means wrd1 comes before wrd2, > 0 after
         if (strcmp(node_ptr->word, lookup_wrd) == 0)
         {
             return true;
@@ -53,38 +60,6 @@ bool check(const char *word)
         node_ptr = node_ptr->next;
     }
     return false;
-}
-
-unsigned int djb2(const char *word)
-{
-    unsigned long hash = 5381;
-    int c = *word;
-    // c = tolower(c); // not needed we are already processing lowercase words
-    while (*word != 0)
-    {
-        hash = ((hash << 5) + hash) + c;
-        c = *word++;
-        // c = tolower(c); // not needed we are already processing lowercase words
-    }
-
-    return hash % N;
-}
-
-// Ref: https://www.reddit.com/r/cs50/comments/1x6vc8/pset6_trie_vs_hashtable/
-unsigned int hashValue(const char *word)
-{
-    unsigned int hash = 0;
-    for (int i = 0, n = strlen(word); i < n; i++)
-    {
-        hash = (hash << 2) ^ word[i];
-    }
-    return hash % N;
-}
-
-// Hashes word to a number
-unsigned int hash(const char *word)
-{
-    return hashValue(word); // change for either hashValue or djb2 to test
 }
 
 bool load(const char *dictionary)
@@ -99,26 +74,17 @@ bool load(const char *dictionary)
 
     while (fscanf(dic, "%s", buffer) != EOF)
     {
-        // create temp node
         node *temp = malloc(sizeof(node));
 
-        // strncpy(wrd1, wrd2, n) copies wrd1 into wrd2 up to n bytes, sizeof(wrd2)>sizeof(wrd1) it fills the rest with /0
-        // as we are declaring buffer as buffer[LENGTH + 1], it will always have at least one /0
         strncpy(temp->word, buffer, sizeof(buffer));
 
-        // all temp node values need to be initialized or valgrind will complain if we use them uninitialized
-        // further reading here: https://cs50.stackexchange.com/questions/18563/conditional-jump-or-move-depends-on-uninitialised-values
-        temp->next = NULL;
-
-        // obtain word index
         int idx = hash(buffer);
 
-        // if table[idx] is NULL (pointing nowhere), set it to point to the temp node
         if (table[idx] == NULL)
         {
+            temp->next = NULL;
             table[idx] = temp;
         }
-        // else append the temp node to the start of the linked list in that table node
         else
         {
             temp->next = table[idx];
@@ -130,7 +96,6 @@ bool load(const char *dictionary)
     return true;
 }
 
-// Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
     return dic_wrds_cnt;

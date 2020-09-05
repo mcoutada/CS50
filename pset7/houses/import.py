f84e61625c9433c201d4a1a7450b6667750e14fb
Mariano <mcoutada@gmail.com>
2020-09-05 16:16:19 -0300

    ...

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -10,7 +10,7 @@ sqlite3
 .header on
 .mode column
 .open "students.db"
-SELECT * FROM students WHERE last IN ('Crabbe','Weasley');
+SELECT * FROM students WHERE last IN ('Longbottom','Weasley');
 """
 
 from csv import DictReader
@@ -25,35 +25,35 @@ def main():
         print("Usage: python import.py characters.csv")
         return
 
-    with connect('students.db') as conn:
+    with connect("students.db") as conn:
 
         # Empty the table in case there's records from previous runs
-        conn.cursor().execute('DELETE FROM students;')
+        conn.cursor().execute("DELETE FROM students;")
 
         # csvFile headers = name,house,birth
         with open(argv[1], "r") as csvFile:
+
             housesDB = DictReader(csvFile)
 
-            for pplRec in housesDB:
-                firstName = ''.join(pplRec["name"].split()[:1])
-                lastName = ''.join(pplRec["name"].split()[-1:])
-                middleName = ''.join(pplRec["name"].split()[1:-1])
-                middleName = 'NULL' if middleName is None else "'" + middleName  + "'" # "'" is equivalent to '\'' which are two valid ways to escape single quotes
+            for row in housesDB:
+                name = row["name"].split()
+                middleName = "".join(name[1:-1])
+                middleName = "NULL" if middleName is None else "'" + middleName  + "'"
 
-        # Triple quotes (both varieties, """ and ''' are permitted) allow the string to contain line breaks
-                sql = """INSERT INTO students ( first
-                                              , middle
-                                              , last
-                                              , house
-                                              , birth
-                                              )
-                                       VALUES ( '{}'
-                                              , {}
-                                              , '{}'
-                                              , '{}'
-                                              , {}
-                                              );
-                      """.format(firstName, middleName, lastName, pplRec["house"], pplRec["birth"])
+                # Triple quotes (both varieties, """ and ''' are permitted) allow the string to contain line breaks
+                sql = f"""INSERT INTO students ( first
+                                               , middle
+                                               , last
+                                               , house
+                                               , birth
+                                               )
+                                        VALUES ( '{name[0]}'
+                                               ,  {middleName}
+                                               , '{name[-1]}'
+                                               , '{row["house"]}'
+                                               ,  {row["birth"]}
+                                               );
+                       """
                 conn.cursor().execute(sql)
             conn.commit()
     return
--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -18,24 +18,48 @@ def main():
     with connect('students.db') as conn:
 
         # Triple quotes (both varieties, """ and ''' are permitted) allow the string to contain line breaks
-        sql = """  SELECT first
-                        , middle
-                        , last
-                        , birth
-                     FROM students
-                    WHERE house = '{hogwartsHouse}'
-                 ORDER BY last, first
-              """.format(hogwartsHouse = argv[1])
+        sql = f"""  SELECT CASE WHEN middle is NULL
+                           THEN
+                               first || ' ' || last
+                           ELSE
+                               first || ' ' || middle || ' ' || last
+                           END
+                         , birth
+                      FROM students
+                     WHERE house = '{argv[1]}'
+                  ORDER BY last, first
+               """
 
         students = conn.execute(sql)
 
         # Iterate through all the students
         for row in students:
-            name = " ".join(row[:3] if row[1] else row[:1] + row[2:3])
-            birth = row[3]
-            print("{}, born {}".format(name, birth))
+            print(f"{row[0]}, born {row[1]}")
 
     return
 
 
 main()
+
+
+# solving them middle name issue from python:
+
+    # with connect('students.db') as conn:
+
+    #     # Triple quotes (both varieties, """ and ''' are permitted) allow the string to contain line breaks
+    #     sql = """  SELECT first
+    #                     , middle
+    #                     , last
+    #                     , birth
+    #                  FROM students
+    #                 WHERE house = '{hogwartsHouse}'
+    #              ORDER BY last, first
+    #           """.format(hogwartsHouse = argv[1])
+
+    #     students = conn.execute(sql)
+
+    #     # Iterate through all the students
+    #     for row in students:
+    #         name = " ".join(row[:3] if row[1] else row[:1] + row[2:3])
+    #         birth = row[3]
+    #         print("{}, born {}".format(name, birth))

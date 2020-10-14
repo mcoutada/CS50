fe998a033c8c94131b0380cd93ebb8f60e5b542f
Mariano <mcoutada@gmail.com>
2020-10-14 10:59:49 -0300

    ...

--- final_project/flasktutorialcoreyschafer/flaskblog.py
+++ final_project/flasktutorialcoreyschafer/flaskblog.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'dcf8c557345a5b3481a481aaad5192d5'
 posts = [
     {
         'author': 'Corey Schafer',
@@ -26,7 +28,19 @@ def hello():
 
 @app.route('/about')
 def about():
-    return render_template("about.html", title = 'About')
+    return render_template("about.html", title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
 
 
 # by adding these 2 lines I don't need to run "flask run", just call the script normally

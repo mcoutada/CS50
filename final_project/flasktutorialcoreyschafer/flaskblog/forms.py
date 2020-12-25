8af33d8924c681ba6aea7a6102dbc0b99a21f68b
Mariano <mcoutada@gmail.com>
2020-12-25 11:44:08 -0300

    ended part 9

--- final_project/flasktutorialcoreyschafer/flaskblog/forms.py
+++ final_project/flasktutorialcoreyschafer/flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -18,18 +18,16 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError(
-                    "That username is taken. Please choose a different one"
-                )
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "That username is taken. Please choose a different one"
+            )
 
     def validate_email(self, email):
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError(
-                    "That email is taken. Please choose a different one"
-                )
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That email is taken. Please choose a different one")
 
 
 class LoginForm(FlaskForm):
@@ -61,4 +59,12 @@ class UpdateAccountForm(FlaskForm):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError("That email is taken. Please choose a different one")
+                raise ValidationError(
+                    "That email is taken. Please choose a different one"
+                )
+
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField('Post')

ea0e2fccf4180a0184384778dec0be9dbef55e39
Mariano <mcoutada@gmail.com>
2020-12-30 20:08:26 -0300

    finished part 10 - email and password reset

--- final_project/flasktutorialcoreyschafer/flaskblog/__init__.py
+++ final_project/flasktutorialcoreyschafer/flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "dcf8c557345a5b3481a481aaad5192d5"
@@ -12,6 +13,42 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
+#### - start - gMail settings ####
+# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+# # if the above doesn't work, use:
+# # app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# # Either use TLS:
+# # app.config['MAIL_PORT'] = 587
+# # app.config['MAIL_USE_TLS'] = True
+# # Or SSL:
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USE_SSL'] = True
+# # Either will need you to set to enable Less Protected Apps in Gmail Account Security
+# # https://myaccount.google.com/security?gar=1
+# app.config['MAIL_USERNAME'] = 'xxxxx@gmail.com'
+# app.config['MAIL_PASSWORD'] = 'xxxxx'
+# #### - end - gMail settings ####
 
-# this import is needed here and not at first part in order to avoid circular import issuepip onstall
+#### - start - other mail settings ####
+app.config['MAIL_SERVER'] = 'mail.xxxxxx.com.ar'
+
+# Either use TLS:
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# Or SSL:
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'xxxxxx'
+app.config['MAIL_PASSWORD'] = 'xxxxxx'
+### - end - other mail settings ####
+
+
+# if setting the user and password as environment variables, then you can use:
+# import os
+# app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+# app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+# this import is needed here and not at first part in order to avoid circular import issue
 from flaskblog import routes
--- final_project/flasktutorialcoreyschafer/flaskblog/forms.py
+++ final_project/flasktutorialcoreyschafer/flaskblog/forms.py
@@ -44,8 +44,9 @@ class UpdateAccountForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     submit = SubmitField("Update")
     picture = FileField(
-        "Update Profile picture", validators=[FileAllowed(["jpg", "png"])]
+        "Update Profile Picture", validators=[FileAllowed(["jpg", "png"])]
     )
+    submit = SubmitField("Update")
 
     def validate_username(self, username):
         if username.data != current_user.username:
@@ -67,4 +68,24 @@ class UpdateAccountForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     content = TextAreaField("Content", validators=[DataRequired()])
-    submit = SubmitField('Post')
+    submit = SubmitField("Post")
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError(
+                "There is no account with that email. You must register first."
+            )
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Reset Password")
